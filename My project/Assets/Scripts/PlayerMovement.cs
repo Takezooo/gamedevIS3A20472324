db5fc3e1d8059646dca333b67ab645afd5c5ee86
Dave Jornales <plusultrastral@gmail.com>
2023-10-02 21:49:37 +0800

    trying to figure out how to jump

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,22 +12,36 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
+        //float v = Input.GetAxis("Vertical");
         Vector2 pos = transform.position;
+        //pos.x += h * speed * Time.deltaTime;
         pos.x += h * speed * Time.deltaTime;
-        pos.y += v * speed * Time.deltaTime * 5;
+
+        if (Input.GetButtonDown("Jump") && pos.y <= -2.51f)
+        {
+            pos.y += Mathf.Sqrt(1 * -3.0f * -9.81f);
+        }
 
         transform.position = pos;
+    }
 
+    //Collision detector
+    /*
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        Debug.Log("Collided");
 
     }
+    */
+
+
 }
+
